74a29cc375043befdab195360cb9bb118e7e0f47
Scott Harrison <scott.harrison25@gmail.com>
2014-11-13 14:33:17 -0600

    Part 2 finished commit

---
+++ app/controllers/finder_controller.rb
@@ -0,0 +1,13 @@
+class FinderController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+  
+  def alphabetized
+    @customers = Customer.order(:full_name)
+  end
+  
+  def missing_email
+    @customers =  Customer.where("email_address = ''")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :customers
-
+  
+  root 'finder#index'
+  get 'missing_email' => 'finder#missing_email'
+  get 'alphabetized' => 'finder#alphabetized'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
